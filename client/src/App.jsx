e47f8c29338df5949d9a178fa371e23f20bd2a4e
Aniket <aniketbaranwal8090@gmail.com>
2025-06-19 00:15:55 +0530

    intial frontend react setup with tailwindcss, shadcn, react-query

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center bg-blue-500 text-white text-xl font-semibold ">
+      EduConnect in progress...
+    </div>
+  )
+}
+
+export default App
