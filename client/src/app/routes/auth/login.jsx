34b846e326c9b3ea22e161aa31108cf42f96b303
Aniket <aniketbaranwal8090@gmail.com>
2025-06-20 19:19:18 +0530

    Added loginForm with zod validation

--- client/src/app/routes/auth/login.jsx
+++ client/src/app/routes/auth/login.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+
+import LoginForm from "@/components/form/auth/loginForm";
+import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "@/components/ui/card";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+     <div className="min-h-screen bg-background flex items-center justify-center">
+      <Card className="backdrop-blur-md rounded-2xl shadow-lg w-full max-w-md">
+        <CardHeader className="pt-8 text-center">
+          <CardTitle className="text-2xl font-semibold">
+            Welcome Back
+          </CardTitle>
+        </CardHeader>
 
-export default Login
+        <CardContent className="px-8 pb-4">
+          <LoginForm />
+        </CardContent>
+
+        <CardFooter className="pb-8 text-center">
+          <p className="text-sm text-muted">
+            New here?{' '}
+            <Link to="/auth/register" className="text-secondary hover:underline">
+              Sign up
+            </Link>
+          </p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default Login;
--- client/src/schema/authSchema.js
+++ client/src/schema/authSchema.js
@@ -1,3 +1,5 @@
+import { z } from "zod";
+
 const passwordSchema = z
   .string()
   .refine((value) => /[0-9]/.test(value), {
