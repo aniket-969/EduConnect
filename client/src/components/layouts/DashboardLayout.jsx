7ff0b6f45710e28f2f7fd5f5a888c902f76acc84
Aniket <aniketbaranwal8090@gmail.com>
2025-06-23 12:19:47 +0530

    Added sidebarProvider and trigger to dashboardlayout

--- client/src/components/layouts/DashboardLayout.jsx
+++ client/src/components/layouts/DashboardLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import  AppSidebar  from '@/components/layouts/Sidebar';
+import { SidebarProvider, SidebarTrigger } from '../ui/sidebar';
 
 export function ErrorBoundary() {
   return <div>Something went wrong in the dashboard.</div>;
@@ -8,11 +9,20 @@ export function ErrorBoundary() {
 
 export default function DashboardLayout() {
   return (
-    <div className="min-h-screen flex">
-      <AppSidebar />
-      <main className="flex-1 p-6">
-        <Outlet />
-      </main>
-    </div>
+     <SidebarProvider>
+      <div className="min-h-screen flex">
+     
+        <AppSidebar />
+
+        <main className="flex-1 p-6 relative">
+         
+          <SidebarTrigger className="md:hidden absolute top-4 left-4 z-10" />
+
+          <div className="pt-10">
+            <Outlet />
+          </div>
+        </main>
+      </div>
+    </SidebarProvider>
   );
 }
