857d07d75ee28008b9f31c1710889795bc8491eb
kavana <kavananavada03@gmail.com>
2025-07-10 13:16:55 +0530

    updated course skeleton

--- client/src/components/instructor/common/CourseSkeleton.jsx
+++ client/src/components/instructor/common/CourseSkeleton.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselPrevious,
   CarouselNext,
-} from '@/components/ui/carousel';
+} from "@/components/ui/carousel";
 
 export default function CourseSkeleton() {
   const skeletons = Array(3).fill(null);
 
   return (
-    <div className="my-5">
+    <div className="my-3">
       <div className="flex items-center justify-between mb-2 px-2">
         <div className="h-6 w-40 bg-card-foreground/5 animate-pulse" />
       </div>
@@ -27,15 +27,18 @@ export default function CourseSkeleton() {
 
                   {/* Content */}
                   <div className="pt-8 pl-4 space-y-2">
-                    <div className="h-4 w-3/4 bg-card-foreground/5 animate-pulse pt-2" />
+                    <div className="h-4 w-3/4 bg-card-foreground/5 animate-pulse pt-4" />
                     <div className="h-3 w-1/2 bg-card-foreground/5 animate-pulse pt-4" />
-                    <div className="h-3 w-1/3 bg-card-foreground/5 animate-pulse pt-4" />
 
-                    {/* Action Icons */}
-                    <div className="flex justify-end gap-4 pt-2 p-4">
-                      <div className="w-5 h-5 bg-card-foreground/5 rounded-sm animate-pulse" />
-                      <div className="w-5 h-5 bg-card-foreground/5 rounded-sm animate-pulse" />
-                      <div className="w-5 h-5 bg-card-foreground/5 rounded-sm animate-pulse" />
+                    <div className="mt-auto flex justify-between items-center">
+                      <div className="h-3 w-1/3 bg-card-foreground/5 animate-pulse pt-4" />
+
+                      {/* Action Icons */}
+                      <div className="flex justify-end gap-4 pt-2 p-4">
+                        <div className="w-5 h-5 bg-card-foreground/5 rounded-sm animate-pulse" />
+                        <div className="w-5 h-5 bg-card-foreground/5 rounded-sm animate-pulse" />
+                        <div className="w-5 h-5 bg-card-foreground/5 rounded-sm animate-pulse" />
+                      </div>
                     </div>
                   </div>
                 </div>
